cc7863f1dec154e262b09c46bdfceaf85d8aab5f
Touathi Vang <Touathi@outlook.com>
2023-04-13 16:15:17 -0500

    Removed supplychange and also pushed for Q. 4 & 5

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -18,14 +18,20 @@ console.log(supplyChange);
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
 
-
+console.log(supplyChange[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
+let removesupplyChange = supplyChange.pop();
+console.log(removesupplyChange);
+
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+
+supplyChange.push(25);
+console.log(supplyChange);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -34,6 +40,11 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 
+for ( let i = 0; i < supplyChange.length; i++) {
+    if(supplyChange[ i ] > 0  ) {
+        console.log('Added',supplyChange[ i ], 'parts');
+    }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
